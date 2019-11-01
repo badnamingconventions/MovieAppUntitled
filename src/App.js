66cf5c82827cf26a636c2dd896759d8818b048d1
DJ <djsm00thee3@gmail.com>
2019-10-31 22:25:06 -0700

    Fixing the name of the auto filter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import NameAutoFiller from "./SearchAutoFiller";
+import SearchAutoFiller from "./SearchAutoFiller";
 import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NameAutoFiller />
+        <SearchAutoFiller />
       </header>
     </div>
   );
