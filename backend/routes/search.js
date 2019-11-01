6fed5fa80f1069fa598eb6ae5dc0149bf3be8e73
DJ <djsm00thee3@gmail.com>
2019-10-31 20:24:47 -0700

    splitting title search into sections

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -4,9 +4,9 @@ const { check, validationResult } = require("express-validator");
 const searchRoutes = express.Router();
 const NameBasic = require("../models/name_basic");
 const TitleBasic = require("../models/title_basic");
-require('dotenv').config()
+require("dotenv").config();
 
-console.log("search env test: "+ process.env.TMDB_API_KEY);
+console.log("search env test: " + process.env.TMDB_API_KEY);
 
 searchRoutes.route("/title").get(function(req, res) {
   TitleBasic.estimatedDocumentCount().then(count => {
@@ -14,31 +14,37 @@ searchRoutes.route("/title").get(function(req, res) {
   });
 });
 
-searchRoutes.get(
-  "/title/:title",
-  [
-    check("title")
-      .trim()
-      .isLength({ min: 3 })
-      .escape()
-  ],
-  function(req, res) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
+const findTitle = title_type => (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
 
-    const title = req.params.title;
-    const query = textSearch(title, { title_type: { $exists: true } });
+  const title = req.params.title;
+  const query = textSearch(title, { title_type });
 
-    console.log("title: " + title);
-    console.log("query: " + JSON.stringify(query));
+  console.log("title: " + title);
+  console.log("query: " + JSON.stringify(query));
 
-    TitleBasic.find(query).limit(25).then(titles => {
+  TitleBasic.find(query)
+    .limit(5)
+    .then(titles => {
       res.json(titles);
     });
-  }
-);
+};
+
+const titleCheck = [
+  check("title")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+];
+
+searchRoutes.get("/title/:title", titleCheck, findTitle({ $exists: true }));
+
+searchRoutes.get("/title/movie/:title", titleCheck, findTitle("movie"));
+
+searchRoutes.get("/title/tv/:title", titleCheck, findTitle("tvEpisode"));
 
 searchRoutes.get(
   "/name/:name",
@@ -60,9 +66,11 @@ searchRoutes.get(
     console.log("name: " + name);
     console.log("query: " + JSON.stringify(query));
 
-    NameBasic.find(query).limit(25).then(names => {
-      res.json(names);
-    });
+    NameBasic.find(query)
+      .limit(5)
+      .then(names => {
+        res.json(names);
+      });
   }
 );
 
